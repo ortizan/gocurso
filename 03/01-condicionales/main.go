b6bb4282034e8d66bcddc243086cd2f6a8656a8f
Antonio Ortiz <info@formacionlibre.com>
2018-05-21 02:32:01 +0200

    Agrega condicionales

---
+++ 03/01-condicionales/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	if isValid := true; isValid {
+		fmt.Println("Esto es verdadero")
+	} else {
+		fmt.Println("Esto es falso")
+	}
+
+	fmt.Println("Fin del programa")
+}
